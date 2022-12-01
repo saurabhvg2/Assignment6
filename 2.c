480a7a5a8717b0480d96355151cc12f1edddce34
Saurabh Gupta <106170495+saurabhvg2@users.noreply.github.com>
2022-12-01 15:43:16 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n, i , sum = 0;
+    printf("enter the n value");
+    scanf("%d",&n);
+    for ( i = 1; i <=n; i++)
+    {
+        sum=sum+i*2;
+    }
+    printf("%d",sum);
+}
